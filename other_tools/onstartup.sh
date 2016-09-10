a0af3501ac77c49ae87bfc5c9c4b9603db85b3ad
Kingshu <kingshu.crusader@gmail.com>
2016-09-10 21:40:35 +0000

    Separated onstartup into scripts

--- other_tools/onstartup.sh
+++ other_tools/onstartup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 cd ~
+echo "Initializing pigpiod"
 sudo pigpiod
 sleep 3
-python ledcontrolserver/led.py
-./ngrok http 8080
+echo "Starting server screen"
+screen -S "server" -d -m "./startServer.sh"
+sleep 5
+echo "Starting ngrok"
+screen -S "ngrok" -d -m "./runngrok.sh"
 sleep 10
+echo "Sending URL via email"
 python sendNgrokUrl.py
